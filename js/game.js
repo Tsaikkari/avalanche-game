55095a6ecf64c70507fddab2e08f361c63d0f03e
Tsaikkari <kirsi.trospe@gmail.com>
2021-11-02 12:19:22 +0100

    freeFall

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ class Game {
     this.backgroundImages
     this.cottage = new Cottage()
     this.cottageImages
-    this.snow1 = new Snow(668, 110, 100)
+    this.snow1 = new Snow(690, 150, 60)
     this.snow2 = new Snow(440, 200, 80)
     this.player = new Player()
     this.started = false
@@ -19,18 +19,7 @@ class Game {
         { src: loadImage('images/cottage-phase2.png') },
         { src: loadImage('images/cottage-phase3.png') },
         { src: loadImage('images/cottage-phase4.png') },
-      ]),
-      document
-        .querySelector('.start-btn')
-        .addEventListener('click', function () {
-          console.log('click')
-          if (!this.started) {
-            this.started === true
-            console.log(this.started) // TODO: fix undefined
-          // this.snow1.freeFall()
-          // this.snow2.freeFall()
-          }
-        })
+      ])
   }
 
   draw() {
@@ -41,4 +30,40 @@ class Game {
     this.snow2.draw()
     this.player.draw()
   }
+
+  calculateDistanceBySnowBall(snow, cottage, background) {
+    let bottom = snow.x - (cottage.x - cottage.width)
+    let right = snow.y - (background.height - cottage.y)
+    let distance = Math.sqrt(Math.pow(bottom, 2) + Math.pow(right, 2))
+    return distance
+  }
+
+  freeFall() {
+    // TODO: loop for snowballs fall, starting randomly from two mountains
+    // Math.floor(Math.random() * 2) + 1
+
+    // if snow1 or snow2 distance to the cottage = 0 and it's the first hit -> cottage2.image
+    // if distance < 0, change z by -1 to stop
+    // else if cottage2 -> cottage3
+    // else cottage4, shake and game over
+    console.log('freeFall')
+    let counter = 0
+    // FIX: This returns NaN
+    console.log(
+      this.calculateDistanceBySnowBall(
+        this.snow1,
+        this.cottage,
+        this.background
+      )
+    )
+  }
 }
+
+document.querySelector('.start-btn').addEventListener('click', function () {
+  console.log('click')
+  if (!this.started) {
+    this.started === true
+    console.log(this.started) // TODO: fix undefined
+    game.freeFall()
+  }
+})
--- js/snow.js
+++ js/snow.js
@@ -1,35 +1,14 @@
 class Snow {
-  constructor(x, y, radius) {
+  constructor(x, y, radius, cottage, background) {
     this.x = x
     this.y = y
     this.radius = radius
+    this.cottage = cottage
+    this.background = background
   }
 
   draw() {
     circle(this.x, this.y, this.radius)
     stroke('#fff')
   }
-
-  freeFall() {
-    // TODO: loop for snowballs fall, starting randomly from two mountains
-   
-  }
-
-  // TODO: figure out the right way to do this
-  moveDiagonal() {
-    this.z -= 1
-  }
-
-  hitCottage() {
-    let counter = 0
-    // TODO: if snow1 or snow2 distance to the cottage is 0 and it's the first hit -> cottage2.image
-    // else if cottage2 -> cottage3
-    // else cottage4, shake and game over
-     
-  }
-
-  onStart() {
-    game.snow.moveDiagonal()
-  }
- 
 }
