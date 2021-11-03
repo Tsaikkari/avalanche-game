8a44f20d4c03da6e2fc1f1ad0e0e9c5ae73ca2ea
Tsaikkari <kirsi.trospe@gmail.com>
2021-11-03 16:29:43 +0100

    Fix countdown

--- js/main.js
+++ js/main.js
@@ -37,49 +37,43 @@ function startOver() {
 function countdown() {
   if (game.started) {
     let sec = 10
-  let countDiv = document.getElementById('timer')
-  let countDown = setInterval(function () {
-    secpass()
-  }, 1000)
+    let countDiv = document.getElementById('timer')
+    let countDown = setInterval(function () {
+      secpass()
+    }, 1000)
 
-  function secpass() {
-    let min = Math.floor(sec / 60)
-    let remSec = sec % 60
+    function secpass() {
+      let min = Math.floor(sec / 60)
+      let remSec = sec % 60
 
-    if (remSec < 10) {
-      remSec = '0' + remSec
-    }
-    if (min < 10) {
-      min = '0' + min
-    }
-    countDiv.innerHTML = min + ':' + remSec
+      if (remSec < 10) {
+        remSec = '0' + remSec
+      }
+      if (min < 10) {
+        min = '0' + min
+      }
+      countDiv.innerHTML = min + ':' + remSec
 
-    if (sec > 0) {
-      sec = sec - 1
-    } else {
-      clearInterval(countDown)
-      if (game.hitcount < 5) {
-        countDiv.innerHTML = 'You Won!'
+      if (sec > 0) {
+        sec = sec - 1
       } else {
-        countDiv.innerHTML = ''
+        clearInterval(countDown)
+        if (game.hitcount < 5) {
+          countDiv.innerHTML = 'You Won!'
+        } else {
+          countDiv.innerHTML = ''
+        }
       }
     }
   }
-  }
-  
 }
 
 document.querySelector('.start-btn').addEventListener('click', function () {
-  if (!game.started) {
-    game.started = true
-    setTimeout(function () {
-      document.querySelector('.start-btn').innerText = 'The Avalanche'
-    }, 1)
-  }
+  countdown()
+  setTimeout(function () {
+    document.querySelector('.start-btn').innerText = 'The Avalanche'
+  }, 1)
 })
-
-// TODO: should start when game starts
-//game.started && countdown()
 
 // Photo Credits:
 // <a href="https://www.vecteezy.com/free-vector/snow-mountain">Snow Mountain Vectors by Vecteezy</a>
