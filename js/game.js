6af2d813637362bd94555553a52a6fd354841136
Tsaikkari <kirsi.trospe@gmail.com>
2021-12-27 16:27:55 +0100

    remove extra

--- js/game.js
+++ js/game.js
@@ -2,27 +2,63 @@ class Game {
   constructor() {
     this.background = new Background()
     this.cottage = new Cottage()
-    this.cottageImages;
+    this.cottageImages
     this.snow1 = new Snow(690, 150, 60)
     this.snow2 = new Snow(440, 200, 80)
     this.player = new Player()
     this.gorge = new Gorge()
-    this.started = false;
-    this.hitcount = 0;
+    this.started = false
+    this.hitcount = 0
   }
 
   preload() {
     ;(this.backgroundImage = loadImage('images/mountains.png')),
       (this.playerImage = loadImage('images/pinkpanther.png')),
       (this.cottageImages = [
-        { src: loadImage('images/cottage.png'), x: 0, y: 400, width: 157, height: 74 },
-        { src: loadImage('images/cottage-phase1.png'), x: 0, y: 400, width: 157, height: 74 },
-        { src: loadImage('images/cottage-phase2.png'), x: 0, y: 400, width: 157, height: 74 },
-        { src: loadImage('images/cottage-phase3.png'), x: 0, y: 400, width: 157, height: 74 },
-        { src: loadImage('images/cottage-phase4.png'), x: 0, y: 400, width: 157, height: 74 },
-        { src: loadImage('images/cottage-phase5.png'), x: 0, y: 400, width: 157, height: 74 },
+        {
+          src: loadImage('images/cottage.png'),
+          x: 0,
+          y: 400,
+          width: 157,
+          height: 74,
+        },
+        {
+          src: loadImage('images/cottage-phase1.png'),
+          x: 0,
+          y: 400,
+          width: 157,
+          height: 74,
+        },
+        {
+          src: loadImage('images/cottage-phase2.png'),
+          x: 0,
+          y: 400,
+          width: 157,
+          height: 74,
+        },
+        {
+          src: loadImage('images/cottage-phase3.png'),
+          x: 0,
+          y: 400,
+          width: 157,
+          height: 74,
+        },
+        {
+          src: loadImage('images/cottage-phase4.png'),
+          x: 0,
+          y: 400,
+          width: 157,
+          height: 74,
+        },
+        {
+          src: loadImage('images/cottage-phase5.png'),
+          x: 0,
+          y: 400,
+          width: 157,
+          height: 74,
+        },
       ]),
-      setTimeout(function() {
+      setTimeout(function () {
         document.querySelector('.start-btn').innerText = 'START'
       }, 2000)
   }
@@ -36,20 +72,4 @@ class Game {
     this.player.draw()
     this.gorge.draw()
   }
-
-  // TODO
-  rotateRight() {
-
-  }
-
-  rotateLeft() {
-
-  }
-
-  shake() {
-    this.cottage.rotateRight()
-    this.cottage.rotateLeft()
-  }
 }
-
-
